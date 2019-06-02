8af2512abb59d161da8cc54480bcca6512026530
ALCeglei <48694927+ALCeglei@users.noreply.github.com>
2019-06-02 16:43:41 +0300

    Add files via upload

---
+++ products/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField(max_length=12)
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
+
+
+class Offer(models.Model):
+    code = models.CharField(max_length=10)
+    description = models.CharField(max_length=250)
+    discount = models.FloatField(max_length=5)
